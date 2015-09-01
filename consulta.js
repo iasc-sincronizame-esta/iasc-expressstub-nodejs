5d5326b6b4ad6e453a414249f7aec6640a83d429
Gigi <giselleariadna@gmail.com>
2015-09-01 20:05:54 -0300

    Nos guardamos el json en el objeto

--- consulta.js
+++ consulta.js
@@ -1,6 +1,11 @@
+_ = require("lodash")
+
 function Consulta(data) {
   this.id = Math.floor(Math.random() * 1000000);
   this.respuestas = [];
+
+  console.log(data);
+  _.assign(this, data);
 }
 
 Consulta.prototype.responder = function(respuesta) {
--- consultasController.js
+++ consultasController.js
@@ -5,19 +5,19 @@ module.exports = function(app) {
   var consultas = []
 
   // POST /consultas { remitente: 'Nahue', mensaje: 'asd' }
-  app.post('consultas', function(req, res) {
+  app.post('/consultas', function(req, res) {
     consultas.push(new Consulta(req.body));
     res.send(200);
   });
 
   // GET /consultas
-  app.get('consultas', function(req, res) {
+  app.get('/consultas', function(req, res) {
       res.json(consultas);
   });
 
 
   // POST /consultas/id/respuestas { remitente: 'Nahue', mensaje: 'asd' }
-  app.post('consultas/:id/respuestas', function(req, res) {
+  app.post('/consultas/:id/respuestas', function(req, res) {
     idRespuesta = req.params.id
 
     respuesta = _.find(consultas, { id: idRespuesta });
