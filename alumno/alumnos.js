6ef9b1b8fe6a400712d9d1335097a3f25461ffba
PalumboN <nahuel.palumbo@gmail.com>
2015-09-14 23:38:10 -0300

    Alumnos que tira requests

--- alumno/alumnos.js
+++ alumno/alumnos.js
@@ -1,3 +1,7 @@
+var baseUrl = "http://192.168.3.67",
+	httpPort = "3000",
+	socketPort = "3001";
+
 var _ = require("lodash");
 var request = require('request');
 var config = require("../shared/config")
@@ -8,48 +12,50 @@ var ayudantesApi = {
 	postConsulta : function (consulta, cb) {
 		request.post({
 			headers: { 'content-type' : 'application/json' },
-			url: baseUrl + 'consultas',
+			url: baseUrl + ':' + httpPort + '/' + 'consultas',
 			body: JSON.stringify(consulta)
 		}, cb);
 	}
 };
 
-var remitentes = ["Aldana", "Rodri", "Ariel", "Charly", "Nahuel"]
-var mensajes = [
-	'¿A qué hora es la clase?',
-	'¿En qué aula es?',
-	'¿Había que hacer un TP para hoy?'
-]
+var remitentes = ["Aldana", "Rodri", "Ariel", "Charly", "Nahuel"];
+
+var consultasFactory = {
+	remitente : _.sample(remitentes),
+	mensajes : [
+		'¿A qué hora es la clase?',
+		'¿En qué aula es?',
+		'¿Había que hacer un TP para hoy?'
+	],
+
+	random : function() {
+		return {
+			remitente: this.remitente,
+			mensaje: _.sample(this.mensajes)
+		};
+	}
+};
 
 
-function sendConsulta() {
 
-	var body = {
-		remitente: _.sample(remitentes),
-		mensaje: _.sample(mensajes)
-	};
+function sendConsultaHttp() {
+
+	var body = consultasFactory.random()
 
 	var cb = function(err, response) {
 		if (err) { return console.log(err); }
 
 		console.log(response.statusCode);
-		console.log("Consulta creada: " + JSON.stringify(body));
+		console.log("Consulta enviada: " + JSON.stringify(body));
 	}
 
 	ayudantesApi.postConsulta(body, cb);	
 };
 
 
+io.on("consulta", function(consulta) { console.log("Nueva consulta: " + consulta) })
 
-function sendPorSocket() {
-
-	io.emit("suscribir", {
-			remitente: _.sample(remitentes),
-			mensaje: _.sample(mensajes)
-		});
-};
-
-io.on("hola", function(res) { console.log("Respuesta: " + res) })
+io.on("respuesta", function(consulta) { console.log("Respondieron una consulta: " + consulta) })
 
 
-setInterval(sendPorSocket, 700);
+setInterval(sendConsultaHttp, 1000);
